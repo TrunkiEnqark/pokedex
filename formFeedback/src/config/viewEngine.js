f8d6ebe6dd0ba8fe73a225fd5f403fdbfef9c921
TAK1ST <trananhkiet21082005@gmail.com>
2024-10-02 11:13:46 +0700

    fix open port for .html

--- formFeedback/src/config/viewEngine.js
+++ formFeedback/src/config/viewEngine.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const configViewEngine = (app) => {
-    app.set('views', path.join('./src', 'views'))
-    app.set('view engine', 'ejs')
+    app.set('views',path.join(__dirname,'../views'));
+    app.set('view engine', 'ejs');
     //config static files
-    app.use(express.static(path.join('./src', 'public')))
+    app.use(express.static(path.join('../src', 'public')));
 
 }
 
--- formFeedback/src/server.js
+++ formFeedback/src/server.js
@@ -1,19 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const path = require('path')
-const configViewEngine = require('./config/viewEngine')
-const webRoutes = require('./config/routes/web')
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./config/routes/web');
 
 const mysql = require('mysql2');
 // Get the client
-const app = express()//express      
-const port = process.env.PORT || 8080
-const hostname = process.env.HOST_NAME
+const app = express();//express      
+const port = process.env.PORT || 8080;
+const hostname = process.env.HOST_NAME;
 
 //config template engine
-configViewEngine(app)
+configViewEngine(app);
 //config static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 //khai bao route
 app.use('/',webRoutes) //chia API routes for easy deleted
 
@@ -21,14 +21,14 @@ app.use('/',webRoutes) //chia API routes for easy deleted
 const connection = mysql.createConnection({
   host: 'localhost',
   port:3306,
-  user: 'root',
-  password:'',
+  user: 'tak',
+  password:'1',
   database: 'FeedBack',
 });
 
 // execute will internally call prepare and query
 connection.execute(
-  'SELECT * FROM Users',
+  'SELECT * FROM User',
   function (err, results, fields) {
     console.log(">>> result = ",results); // results contains rows returned by server
     // console.log(fields); // fields contains extra meta data about results, if available
